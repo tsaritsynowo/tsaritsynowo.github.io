9d17bbe55d4594d7f1b304b3bd66a781ed8d2e7a
tsaritsyn <milknnhnx@gmailcom>
2025-03-28 01:55:59 +0800

    new build

--- app/root.jsx
+++ app/root.jsx
@@ -1,19 +1,13 @@
 import { Links, Outlet } from "react-router";
 import profilepic from "../public/profile.jpg";
+import "/public/CSS/index.css"
+import "/public/CSS/project.css"
 
 export function links() {
   return [
     {
       rel: "stylesheet",
       href:"https://lf3-cdn-tos.bytecdntp.com/cdn/expire-1-M/font-awesome/4.7.0/css/font-awesome.min.css"
-    },
-    {
-      rel: "stylesheet",
-      href:"./app/CSS/index.css"
-    },
-    {
-      rel: "stylesheet",
-      href:"./app/CSS/project.css"
     }
   ]};
 
